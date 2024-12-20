7ac329c5477596415188573b9fd858c9b372f99f
Zayd Ajjaji <zayd.ajjaji@student.junia.com>
2024-12-20 13:35:35 +0100

    nettoyage

--- src/main/java/com/prime/projet/exception/GlobalExceptionHandler.java
+++ src/main/java/com/prime/projet/exception/GlobalExceptionHandler.java
@@ -2,8 +2,6 @@ package com.prime.projet.exception;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
